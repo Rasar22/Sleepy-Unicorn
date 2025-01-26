f4010af34292d61f21ab4f66aaf5446fa1a49c76
Rasar22 <radar22.rs@gmail.com>
2025-01-26 11:11:18 +0000

    Start setup for ninja templates

--- venv/Sleepy_unicorn_app/sleepy_unicorn.py
+++ venv/Sleepy_unicorn_app/sleepy_unicorn.py
@@ -3,8 +3,13 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template('index.html')
+def index():
+    return render_template('index.html', title="Home")
+
+@app.route('/start-here')
+def start_here():
+    return render_template('start_here.html', title="Start Here")
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
