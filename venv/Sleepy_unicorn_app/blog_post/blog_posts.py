c0612c1a7c563056ec035cb5d1cfedd334d70d6d
Rasar22 <radar22.rs@gmail.com>
2025-01-30 18:54:03 +0000

    content in blog_post

---
+++ venv/Sleepy_unicorn_app/blog_post/blog_posts.py
@@ -0,0 +1,14 @@
+blog_posts = [
+    {
+        "id": 1,
+        "title": "First Blog Post",
+        "date": "2025-01-28",
+        "content": "This is the content of the first blog post.",
+    },
+    {
+        "id": 2,
+        "title": "Second Blog Post",
+        "date": "2025-01-29",
+        "content": "This is the content of the second blog post.",
+    },
+]
