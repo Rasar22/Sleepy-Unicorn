1e61628e47dd3d9d155062a0acaa0849d1dc6c73
Rasar22 <radar22.rs@gmail.com>
2025-01-30 18:49:03 +0000

    setting new route to app

--- venv/Sleepy_unicorn_app/sleepy_unicorn.py
+++ venv/Sleepy_unicorn_app/sleepy_unicorn.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from blog_posts import blog_posts
 
 app = Flask(__name__)
 
@@ -11,9 +12,16 @@ def index():
 def start_here():
     return render_template('start_here.html', title="Start Here")
 
-@app.route('/blog')
+@app.route("/blog")
 def blog():
-    return render_template('blog.html')
+    return render_template("blog.html", posts=blog_posts)
+
+@app.route("/blog/<int:post_id>")
+def blog_post(post_id):
+    post = next((p for p in blog_posts if p["id"] == post_id), None)
+    if not post:
+        return "Post not found", 404
+    return render_template("blog_post.html", post=post)
 
 @app.route('/about')
 def about():
