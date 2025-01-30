d06099fbfd29837e041c5e056a602eb893af4e52
Rasar22 <radar22.rs@gmail.com>
2025-01-30 18:23:01 +0000

    add blog page addres to app

--- venv/Sleepy_unicorn_app/sleepy_unicorn.py
+++ venv/Sleepy_unicorn_app/sleepy_unicorn.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# page adresses
 @app.route('/')
 def index():
     return render_template('index.html', title="Home")
@@ -10,6 +11,10 @@ def index():
 def start_here():
     return render_template('start_here.html', title="Start Here")
 
+@app.route('/blog')
+def about():
+    return render_template('blog.html')
+
 @app.route('/about')
 def about():
     return render_template('about.html')
@@ -17,6 +22,8 @@ def about():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+
 
    
 if __name__ == '__main__':
