ed3057bda2480a8d91a9a8cf3703538a538af10e
Rasar22 <142432240+Rasar22@users.noreply.github.com>
2025-03-16 16:49:34 +0000

    Change in blog posts/py

--- venv/Sleepy_unicorn_app/blog_posts.py
+++ venv/Sleepy_unicorn_app/blog_posts.py
@@ -11,4 +11,10 @@ blog_posts = [
         "date": "2025-01-29",
         "content": "This is the content of the second blog post.",
     },
+    {
+        "id": 3,
+        "title": "Third",
+        "date": "2025-03-16",
+        "content": "Now I shoud start",   
+    }
 ]
